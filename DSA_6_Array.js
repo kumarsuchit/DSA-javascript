ffbb3a3550c0da9f3c313c92e8d780f63cb8361e
Suchit Kumar <suchit.kumar@dataflowgroup.com>
2026-09-07 16:38:05 +0530

    DSA questions

--- DSA_6_Array.js
+++ DSA_6_Array.js
@@ -253,3 +253,54 @@ function getMaxProfit(arr) {
 console.log("Max profit to buy and sell stock=>", getMaxProfit([7, 1, 5, 3, 6, 4]));
 console.log("Max profit to buy and sell stock=>", getMaxProfit([7, 6, 4, 3, 1]));
 console.log("Max profit to buy and sell stock=>", getMaxProfit([3, 3, 5, 0, 0, 3, 1, 4]));
+
+// Q-9- leetcode-122 => You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+
+// On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can sell and buy the stock multiple times on the same day, ensuring you never hold more than one share of the stock.
+
+// Find and return the maximum profit you can achieve.
+
+// Example 1:
+
+// Input: prices = [7,1,5,3,6,4]
+// Output: 7
+// Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+// Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+// Total profit is 4 + 3 = 7.
+// Example 2:
+
+// Input: prices = [1,2,3,4,5]
+// Output: 4
+// Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+// Total profit is 4.
+// Example 3:
+
+// Input: prices = [7,6,4,3,1]
+// Output: 0
+// Explanation: There is no way to make a positive profit, so we never buy the stock to achieve the maximum profit of 0.
+
+function getMaxProfitSum(arr) {
+    let profit = 0;
+    let totalProfit = 0;
+    let buy = arr[0];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < buy) {
+            buy = arr[i];
+        };
+
+        let checkProfiVal = arr[i] - buy;
+        if (checkProfiVal > profit) {
+            profit = checkProfiVal;
+            totalProfit += profit;
+            buy = arr[i];
+            profit = 0;
+        }
+    }
+
+    console.log("total profit to buy and sell stock=>", totalProfit);
+}
+
+getMaxProfitSum([7, 1, 5, 3, 6, 4]);
+getMaxProfitSum([1, 2, 3, 4, 5]);
+getMaxProfitSum([7, 6, 4, 3, 1]);
